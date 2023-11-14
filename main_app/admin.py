c7797637f1865a3801b32870580e7b48d7a861b5
dm <dalavaro@gmail.com>
2023-11-14 23:36:00 +0300

    20.1

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from main_app.models import Product
 
-# Register your models here.
+
+# admin.site.register(Product)
+
+@admin.register(Product)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'descriptions', )
--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 
-class Product(models.Model):
 
-    pass
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Название')
+    descriptions = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='product/', verbose_name='Фотографии', **NULLABLE)
+    category = models.CharField(max_length=255, verbose_name='Категория')
+    price = models.FloatField(verbose_name='Цена')
+    date = models.DateField(verbose_name='Дата создания')
+    last_date = models.DateField(verbose_name='Дата изменения')
+
+    is_active = models.BooleanField(default=True, verbose_name='Опубликовано')
+
+    def __str__(self):
+        return f'{self.name} {self.category}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('name',)
 
 
 class Category(models.Model):
