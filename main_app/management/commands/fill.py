fee1db37bd6bfd30bf390bbaeb4db368ae6b9243
dm <dalavaro@gmail.com>
2023-11-16 22:14:36 +0300

    20.1

--- main_app/management/commands/fill.py
+++ main_app/management/commands/fill.py
@@ -6,9 +6,21 @@ from main_app.models import Product, Category
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        category_one = Category.objects.create(name='Узорчатые свечи', descriptions='Узорчатые свечи')
-        category_two = Category.objects.create(name='Рождественские свечи', descriptions='Свечи на рождество')
-        category_three = Category.objects.create(name='Пасхальные свечи', descriptions='Свечи на пасху')
+
+        category_one, created_one = Category.objects.get_or_create(
+            name='Узорчатые свечи',
+            defaults={'descriptions': 'Узорчатые свечи'}
+        )
+
+        category_two, created_two = Category.objects.get_or_create(
+            name='Рождественские свечи',
+            defaults={'descriptions': 'Свечи на рождество'}
+        )
+
+        category_three, created_three = Category.objects.get_or_create(
+            name='Пасхальные свечи',
+            defaults={'descriptions': 'Свечи на пасху'}
+        )
 
         product_list = [
             {'name': 'Свеча восковая в коробочке, узор "Дамасский"', 'descriptions': 'Артикул: 101',
@@ -28,6 +40,11 @@ class Command(BaseCommand):
         products_for_create = []
         for product_item in product_list:
             product_item['photo'] = f'product/{product_item["photo"]}' if product_item["photo"] else ''
-            products_for_create.append(Product(**product_item))
-
-        Product.objects.bulk_create(products_for_create)
+            product, created = Product.objects.get_or_create(
+                name=product_item['name'],
+                descriptions=product_item['descriptions'],
+                category=product_item['category'],
+                defaults={'photo': product_item['photo'], 'price': product_item['price']}
+            )
+            if created:
+                products_for_create.append(product)
