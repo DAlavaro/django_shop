9908314d7a13c10ad90c55b2c4de3ed6a2b783bd
dm <dalavaro@gmail.com>
2023-11-20 23:39:38 +0300

    20.2

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 
+from main_app.models import Product
+
+menu = [
+    {'title': 'Инфо', 'url_name': 'info'},
+    {'title': 'О нас', 'url_name': 'about'},
+    {'title': 'Доставка', 'url_name': 'delivery'},
+    {'title': 'Отзывы', 'url_name': 'reviews'},
+]
 def index(request):
-    if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        message = request.POST['message']
-        print(name, email, message)
-    return render(request, 'main_app/index.html')
+    context = {
+        'object_list': Product.objects.all(),
+        'title': 'О сайте',
+        'menu': menu,
+    }
+    return render(request, 'main_app/index.html', context=context)
 
 
 def about(request):
